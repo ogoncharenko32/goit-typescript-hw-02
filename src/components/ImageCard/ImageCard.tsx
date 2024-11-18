c1ed26d678047127a89be763e596badaa093f085
ogoncharenko32 <krapniknil32@gmail.com>
2024-11-18 16:49:57 +0200

    homework done

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,12 +1,17 @@
 type ImageCardParams = {
   imageSrc: string;
-  imageAlt: string;
+  imageAlt: string | null;
 };
 
 const ImageCard = ({ imageSrc, imageAlt }: ImageCardParams) => {
   return (
     <div>
-      <img src={imageSrc} alt={imageAlt} width="400px" height="266px" />
+      <img
+        src={imageSrc}
+        alt={imageAlt || 'Image'}
+        width="400px"
+        height="266px"
+      />
     </div>
   );
 };
